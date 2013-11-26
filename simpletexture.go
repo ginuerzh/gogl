3cae8afc3e50dfef28e4cc3f4ba931d671575253
ginuerzh <ginuerzh@gmail.com>
2013-11-26 08:20:02 +0800

    updated

--- simpletexture.go
+++ simpletexture.go
@@ -3,7 +3,8 @@ package main
 
 import (
 	"fmt"
-	gl "github.com/chsc/gogl/gl43"
+	gl "github.com/chsc/gogl/gl42"
+	//gogl "github.com/go-gl/gl"
 	glfw "github.com/go-gl/glfw3"
 	"log"
 )
@@ -26,9 +27,10 @@ func getShaderInfoLog(s gl.Uint) string {
 	var length gl.Int
 	gl.GetShaderiv(s, gl.INFO_LOG_LENGTH, &length)
 	if length > 0 {
-		info := make([]byte, length)
-		gl.GetShaderInfoLog(s, gl.Sizei(length), nil, gl.GLString(string(info)))
-		return string(info)
+		info := gl.GLStringAlloc(gl.Sizei(length))
+		defer gl.GLStringFree(info)
+		gl.GetShaderInfoLog(s, gl.Sizei(length), nil, info)
+		return gl.GoString(info)
 	}
 
 	return ""
@@ -38,29 +40,29 @@ func getProgramInfoLog(program gl.Uint) string {
 	var length gl.Int
 	gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &length)
 	if length > 0 {
-		info := make([]byte, length)
-		gl.GetProgramInfoLog(program, gl.Sizei(length), nil, gl.GLString(string(info)))
-		return string(info)
+		info := gl.GLStringAlloc(gl.Sizei(length))
+		defer gl.GLStringFree(info)
+		gl.GetProgramInfoLog(program, gl.Sizei(length), nil, info)
+		return gl.GoString(info)
 	}
 
 	return ""
 }
 
 func compileShaders() gl.Uint {
-	vss := `#version 430
+	vss := `#version 430 core
 	
 			void main(void)
 			{
 				const vec4 vertices[] = vec4[](
 					vec4(0.75, -0.75, 0.5, 1.0),
 					vec4(-0.75, -0.75, 0.5, 1.0),
-					vec4( 0.75,  0.75, 0.5, 1.0)
-				);
+					vec4( 0.75,  0.75, 0.5, 1.0));
 				
 				gl_Position = vertices[gl_VertexID];
 			}`
 
-	fss := `#version 430
+	fss := `#version 430 core
 	
 			uniform sampler2D s;
 			
@@ -70,7 +72,29 @@ func compileShaders() gl.Uint {
 			{
 				color = texture(s, gl_FragCoord.xy / textureSize(s, 0));
 			}`
+	/*
+		vs := gogl.CreateShader(gogl.VERTEX_SHADER)
+		vs.Source(vss)
+		vs.Compile()
+		defer vs.Delete()
+		log.Println("vertex shader info log:", vs.GetInfoLog())
 
+		fs := gogl.CreateShader(gogl.FRAGMENT_SHADER)
+		fs.Source(fss)
+		fs.Compile()
+		defer fs.Delete()
+		log.Println("frag shader info log:", fs.GetInfoLog())
+
+		program := gogl.CreateProgram()
+		program.AttachShader(vs)
+		program.AttachShader(fs)
+		program.Link()
+		program.Use()
+
+		program.Validate()
+		log.Println("validate status:", program.Get(gl.VALIDATE_STATUS))
+		log.Println("program info log:", program.GetInfoLog())
+	*/
 	vs := gl.CreateShader(gl.VERTEX_SHADER)
 	cs := gl.GLString(vss)
 	slen := gl.Int(len(vss))
@@ -83,6 +107,14 @@ func compileShaders() gl.Uint {
 	cs = gl.GLString(fss)
 	slen = gl.Int(len(fss))
 	gl.ShaderSource(fs, 1, &cs, &slen)
+	/*
+		size := 1024
+		var length gl.Sizei = 0
+		c := gl.GLStringAlloc(gl.Sizei(size))
+		gl.GetShaderSource(fs, gl.Sizei(size), &length, c)
+		log.Println(len(fss), length, gl.GoString(c))
+		gl.GLStringFree(c)
+	*/
 	gl.CompileShader(fs)
 	defer gl.DeleteShader(fs)
 	log.Println("frag shader info log:", getShaderInfoLog(fs))
@@ -98,9 +130,11 @@ func compileShaders() gl.Uint {
 	var status gl.Int
 	gl.GetProgramiv(program, gl.VALIDATE_STATUS, &status)
 	log.Println("validate status:", status)
+
+	//Validation warning! - Sampler value s has not been set.
 	log.Println("program info log:", getProgramInfoLog(program))
 
-	return program
+	return gl.Uint(program)
 }
 
 func genTexture(width, height int) []float32 {
@@ -121,6 +155,7 @@ func genTexture(width, height int) []float32 {
 func startup() {
 	log.Println(gl.GoStringUb(gl.GetString(gl.VERSION)))
 
+	gl.ActiveTexture(gl.TEXTURE0 + 1)
 	gl.GenTextures(1, &texture)
 	gl.BindTexture(gl.TEXTURE_2D, texture)
 	gl.TexStorage2D(gl.TEXTURE_2D, gl.Sizei(8), gl.RGBA32F, gl.Sizei(256), gl.Sizei(256))
@@ -144,6 +179,8 @@ func render() {
 	green := []gl.Float{0.0, 0.25, 0.0, 1.0}
 	gl.ClearBufferfv(gl.COLOR, 0, &green[0])
 
+	gl.UseProgram(program)
+
 	gl.DrawArrays(gl.TRIANGLES, 0, 3)
 }
 
@@ -163,9 +200,12 @@ func main() {
 	defer window.Destroy()
 
 	window.MakeContextCurrent()
+	glfw.SwapInterval(1)
 
 	r := gl.Init()
 	log.Println("init opengl:", r)
+
+	//gogl.Init()
 
 	startup()
 
